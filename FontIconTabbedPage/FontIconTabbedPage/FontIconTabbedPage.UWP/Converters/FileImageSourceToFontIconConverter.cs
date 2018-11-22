fef51f12bb265c8de8e73c0fc32ba6cba019b505
tomohak <tomohak@outlook.com>
2018-11-22 15:00:45 +0900

    Make it possible to use unicode point for Icon property (UWP)

---
+++ FontIconTabbedPage/FontIconTabbedPage/FontIconTabbedPage.UWP/Converters/FileImageSourceToFontIconConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace FontIconTabbedPage.UWP.Converters
+{
+    public class FileImageSourceToFontIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Xamarin.Forms.FileImageSource)
+            {
+                var point = (string)((Xamarin.Forms.FileImageSource)value);
+
+                if (int.TryParse(point, NumberStyles.HexNumber, null, out int number))
+                {
+                    string s = Char.ConvertFromUtf32(number);
+                    return new FontIcon() { FontFamily = new FontFamily("Segoe MDL2 Assets"), Glyph = s };
+                }
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FontIconTabbedPage/FontIconTabbedPage/FontIconTabbedPage.UWP/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,23 @@
+﻿using FontIconTabbedPage.UWP.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabbedPageRenderer))]
+namespace FontIconTabbedPage.UWP.Renderers
+{
+    public class CustomTabbedPageRenderer : TabbedPageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Windows.UI.Xaml.DataTemplate template = App.Current.Resources["TabbedPageHeaderTemplate"] as Windows.UI.Xaml.DataTemplate;
+                if (template != null)
+                    Control.HeaderTemplate = template;                        
+            }
+
+        }
+    }
+}
