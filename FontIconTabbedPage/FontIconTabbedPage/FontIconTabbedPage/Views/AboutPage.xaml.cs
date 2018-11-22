6e91c9ab253494839b18862841385db56dde5126
tomohak <tomohak@outlook.com>
2018-11-22 10:58:18 +0900

    Add FontIconTabbedPage project

---
+++ FontIconTabbedPage/FontIconTabbedPage/FontIconTabbedPage/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FontIconTabbedPage.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
